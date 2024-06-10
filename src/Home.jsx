3e1187e5634cfba1cda26410e19707f6d2d1c99f
robotanh <robotanh15@gmail.com>
2024-06-10 21:42:28 +0700

    dev home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,38 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { getDatabase, ref, onValue } from 'firebase/database';
 import { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill } from 'react-icons/bs';
-import { Line } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-  TimeScale,
-  Filler
-} from 'chart.js';
-import 'chartjs-adapter-date-fns';
 import { initializeApp } from 'firebase/app';
-import { ResponsiveContainer } from 'recharts';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
   Tooltip,
   Legend,
-  TimeScale,
-  Filler
-);
+  ResponsiveContainer
+} from 'recharts';
 
 function Home() {
   const [config, setConfig] = useState(null);
   const [sensorData, setSensorData] = useState({});
+  const [signal, setSignal] = useState('STOP'); // Initial value is 'STOP'
 
   useEffect(() => {
     fetch('/firebaseConfig.json') // Assuming you have a firebaseConfig.json file for Firebase configuration
@@ -58,62 +42,27 @@ function Home() {
 
           setSensorData((prevData) => ({
             ...prevData,
-            [path]: [...(prevData[path] || []), { x: new Date(), y: parsedData }],
+            [path]: [...(prevData[path] || []), { x: new Date(), ...parsedData }],
           }));
+
+            // Update signal based on parsedData
+          if (parsedData.so_lit_da_bom_rt === 0) {
+            setSignal('STOP');
+          } else {
+            setSignal('RUN');}
         });
       });
     }
   }, [config]);
+  useEffect(() => {
+
+  }, []);
 
   const parseSensorData = (message) => {
     const so_lit_da_bom_rt = parseFloat(message.substring(10, 19).trim());
     const tien_dang_ban_rt = parseFloat(message.substring(34, 43).trim());
 
     return { so_lit_da_bom_rt, tien_dang_ban_rt };
-  };
-
-  const options = {
-    scales: {
-      x: {
-        type: 'time',
-        time: {
-          unit: 'minute',
-          displayFormats: {
-            minute: 'h:mm a',
-          },
-          color: 'rgba(255, 255, 255, 0.7)', // Set the color of the x-axis text
-        },
-        ticks: {
-          color: 'rgba(255, 255, 255, 0.7)', // Set the color of the x-axis ticks
-        },
-        grid: {
-          color: 'rgba(255, 255, 255, 0.1)', // Set the color of the x-axis grid lines
-        },
-      },
-      y: {
-        title: {
-          display: true,
-          text: 'Đơn Vị',
-          color: 'rgba(255, 255, 255, 0.7)', // Set the color of the y-axis text
-        },
-        ticks: {
-          color: 'rgba(255, 255, 255, 0.7)', // Set the color of the y-axis ticks
-        },
-        grid: {
-          color: 'rgba(255, 255, 255, 0.1)', // Set the color of the y-axis grid lines
-        },
-      },
-    },
-    plugins: {
-      tooltip: {
-        titleColor: 'rgba(255, 255, 255, 0.9)', // Set the color of the tooltip title
-        bodyColor: 'rgba(255, 255, 255, 0.9)', // Set the color of the tooltip body text
-        backgroundColor: 'rgba(0, 0, 0, 0.7)', // Set the background color of the tooltip
-        padding: 10, // Add padding to the tooltip
-        borderColor: 'rgba(255, 255, 255, 0.5)', // Set the border color of the tooltip
-        borderWidth: 1, // Set the border width of the tooltip
-      },
-    },
   };
 
   useEffect(() => {
@@ -153,86 +102,71 @@ function Home() {
             <h3>ALERTS</h3>
             <BsFillBellFill className='card_icon' />
           </div>
-          <h1>42</h1>
+          <h3>{signal}</h3>
         </div>
       </div>
 
       <div className='charts'>
         <ResponsiveContainer width="100%" height="100%">
-          <Line
-            data={{
-              datasets: [
-                {
-                  label: "Số Lít Đã Bơm (Lít)",
-                  data: (sensorData[config?.path[0]] || []).map((dataPoint) => ({
-                    x: dataPoint.x,
-                    y: dataPoint.y.so_lit_da_bom_rt,
-                  })),
-                  borderColor: 'rgba(240, 192, 64, 1)',  // Gold color
-                  borderWidth: 2,  // Slightly thicker border for better visibility
-                  backgroundColor: 'rgba(240, 192, 64, 0.2)',  // Gold with transparency for the fill
-                  pointBackgroundColor: 'rgba(240, 192, 64, 1)',  // Gold color for data points
-                  pointBorderColor: '#fff',  // White border for data points for better contrast
-                  pointHoverBackgroundColor: '#fff',  // White background on hover
-                  pointHoverBorderColor: 'rgba(240, 192, 64, 1)',  // Gold border on hover
-                  fill: true,  // Enable fill for the area under the line
-                }
-              ],
-            }}
-            options={{
-              ...options,
-              scales: {
-                ...options.scales,
-                y: {
-                  ...options.scales.y,
-                  title: {
-                    ...options.scales.y.title,
-                    text: 'Lít',
-                  },
-                },
-              },
-            }}
-          />
-        </ResponsiveContainer>
-        <ResponsiveContainer width="100%" height="100%">
-          <Line
-            data={{
-              datasets: [
-                
-                {
-                  label: "Tiền Đang Bán (đồng)",
-                  data: (sensorData[config?.path[0]] || []).map((dataPoint) => ({
-                    x: dataPoint.x,
-                    y: dataPoint.y.tien_dang_ban_rt,
-                  })),
-                  borderColor: 'rgba(240, 192, 64, 1)',  // Gold color
-                  borderWidth: 2,  // Slightly thicker border for better visibility
-                  backgroundColor: 'rgba(240, 192, 64, 0.2)',  // Gold with transparency for the fill
-                  pointBackgroundColor: 'rgba(240, 192, 64, 1)',  // Gold color for data points
-                  pointBorderColor: '#fff',  // White border for data points for better contrast
-                  pointHoverBackgroundColor: '#fff',  // White background on hover
-                  pointHoverBorderColor: 'rgba(240, 192, 64, 1)',  // Gold border on hover
-                  fill: true,  // Enable fill for the area under the line
-                },
-              ],
-            }}
-            options={{
-              ...options,
-              scales: {
-                ...options.scales,
-                y: {
-                  ...options.scales.y,
-                  title: {
-                    ...options.scales.y.title,
-                    text: 'Đồng',
-                  },
-                },
-              },
-            }}
-          />
+          <LineChart width={500}
+                    height={300}
+                    margin={{
+                      top: 5,
+                      right: 30,
+                      left: 20,
+                      bottom: 5,
+                    }}
+                    data={sensorData[config?.path[0]]}>
+
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="x" />
+            <YAxis yAxisId="so_lit_da_bom_rt" label={{ value: 'Lít', angle: -90, position: 'insideLeft' }} />
+            <Tooltip
+          formatter={(value, name, props) => [`${value} Lít`, new Date(props.payload.x).toLocaleString()]} // Displaying the value and date
+        />
+            <Legend />
+            <Line
+              yAxisId="so_lit_da_bom_rt"
+              type="monotone"
+              dataKey="so_lit_da_bom_rt"
+              name="Số lít đã bơm"
+              stroke="rgba(240, 192, 64, 1)"
+              fill="rgba(240, 192, 64, 0.2)"
+              dot={{ fill: 'rgba(240, 192, 64, 1)' }}
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
         </ResponsiveContainer>
 
-       
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart width={500}
+                      height={300}
+                      margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                      }}
+                      data={sensorData[config?.path[0]]}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="x" />
+            <YAxis yAxisId="tien_dang_ban_rt" label={{ value: 'Đồng', angle: -90, position: 'insideLeft' }} />
+            <Tooltip
+          formatter={(value, name, props) => [`${value} Lít`, new Date(props.payload.x).toLocaleString()]} // Displaying the value and date
+        />
+            <Legend />
+            <Line
+              yAxisId="tien_dang_ban_rt"
+              type="monotone"
+              dataKey="tien_dang_ban_rt"
+              name="Tiền Đang Bán"
+              stroke="rgba(240, 192, 64, 1)"
+              fill="rgba(240, 192, 64, 0.2)"
+              dot={{ fill: 'rgba(240, 192, 64, 1)' }}
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     </main>
   );
