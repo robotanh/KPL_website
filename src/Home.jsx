7ffb0b7b3513ebbac1934d741dd647b6d9c002ce
robotanh <robotanh15@gmail.com>
2024-06-13 11:06:48 +0700

    mutiple path display

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getDatabase, ref, onValue } from 'firebase/database';
 import { FaGasPump } from "react-icons/fa6";
-import { PiIdentificationCard , PiMoneyWavy  } from "react-icons/pi";
+import { PiIdentificationCard, PiMoneyWavy } from "react-icons/pi";
 import { SiRainmeter } from "react-icons/si";
 import { initializeApp } from 'firebase/app';
 import {
@@ -15,21 +15,13 @@ import {
   ResponsiveContainer
 } from 'recharts';
 
-
 function Home() {
   const [config, setConfig] = useState(null);
-  const [sensorData, setSensorData] = useState({
-    'gas_pump_data/Voi_1/raw_data': [
-      { x: new Date().toLocaleDateString(), so_lit_da_bom_rt: 0, tien_dang_ban_rt: 0 }
-    ]
-  });
-  const [signal, setSignal] = useState('STOP'); // Initial value is 'STOP'
-  const [idvoi, setIdvoi] = useState('');
-  const [TienDangBan, setTienDangBan] = useState('');
-  const [SoLitDaBom, setSoLitDaBom] = useState(''); 
+  const [sensorData, setSensorData] = useState({});
+  const [latestData, setLatestData] = useState({});
 
   useEffect(() => {
-    fetch('/firebaseConfig.json') // Assuming you have a firebaseConfig.json file for Firebase configuration
+    fetch('/firebaseConfig.json')
       .then((response) => response.json())
       .then((data) => setConfig(data))
       .catch((error) => console.error('Error loading configuration:', error));
@@ -52,30 +44,22 @@ function Home() {
 
           setSensorData((prevData) => ({
             ...prevData,
-            [path]: [...(prevData[path] || []), { x: new Date(), ...parsedData }],
+            [path]: [...(prevData[path] || []), { x: new Date().getTime(), ...parsedData }],
           }));
 
-            // Update signal based on parsedData
-          if (parsedData.so_lit_da_bom_rt === 0) {
-            setSignal('STOP');
-          } else {
-            setSignal('RUN');
-          }
-          setIdvoi(parsedData.id_voi);
-          setSoLitDaBom(parsedData.so_lit_da_bom_rt);
-          setTienDangBan(parsedData.tien_dang_ban_rt);
+          setLatestData((prevData) => ({
+            ...prevData,
+            [path]: parsedData
+          }));
         });
       });
     }
   }, [config]);
-  useEffect(() => {
-
-  }, []);
 
   const parseSensorData = (message) => {
     const so_lit_da_bom_rt = parseFloat(message.substring(10, 19).trim());
     const tien_dang_ban_rt = parseFloat(message.substring(34, 43).trim());
-    const id_voi = message.charCodeAt(3); // Get the byte 3 as char code
+    const id_voi = message.charCodeAt(3);
 
     return { so_lit_da_bom_rt, tien_dang_ban_rt, id_voi };
   };
@@ -90,92 +74,93 @@ function Home() {
         <h3>DASHBOARD</h3>
       </div>
 
-
-      <div className='main-cards'>
-        <div className='card'>
-          <div className='card-inner'>
-            <h3>Tiền</h3>
-            <PiMoneyWavy className='card_icon' />
+      {config && config.path.map((path, index) => (
+        <div key={index}>
+          <div className='main-cards'>
+            <div className='card'>
+              <div className='card-inner'>
+                <PiMoneyWavy className='card_icon' />
+              </div>
+              <h3>Tiền: {latestData[path]?.tien_dang_ban_rt || 0}</h3>
+            </div>
+            <div className='card'>
+              <div className='card-inner'>
+                <h3>Lít xăng</h3>
+                <SiRainmeter className='card_icon' />
+              </div>
+              <h3>{latestData[path]?.so_lit_da_bom_rt || 0}</h3>
+            </div>
+            <div className='card'>
+              <div className='card-inner'>
+                <h3>ID Vòi</h3>
+                <PiIdentificationCard className='card_icon' />
+              </div>
+              <h3>{latestData[path]?.id_voi || 0}</h3>
+            </div>
+            <div className='card'>
+              <div className='card-inner'>
+                <h3>Trạng Thái</h3>
+                <FaGasPump className='card_icon' />
+              </div>
+              <h3>{latestData[path]?.so_lit_da_bom_rt === 0 ? 'STOP' : 'RUN'}</h3>
+            </div>
           </div>
-          <h3>{TienDangBan}</h3>
-        </div>
-        <div className='card'>
-          <div className='card-inner'>
-            <h3>Lít xăng</h3>
-            <SiRainmeter className='card_icon' />
-          </div>
-          <h3>{SoLitDaBom}</h3>
-        </div>
-        <div className='card'>
-          <div className='card-inner'>
-            <h3>ID Vòi</h3>
-            <PiIdentificationCard className='card_icon' />
-          </div>
-          <h3>{idvoi}</h3>
-        </div>
-        <div className='card'>
-          <div className='card-inner'>
-            <h3>Trạng Thái</h3>
-            <FaGasPump className='card_icon' />
-          </div>
-          <h3>{signal}</h3>
-        </div>
-      </div>
 
+          <div className='charts'>
+            <ResponsiveContainer width="100%" height={400}>
+              <LineChart data={sensorData[path] || []}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis
+                  dataKey="x"
+                  tickFormatter={(tick) => new Date(tick).toLocaleDateString()}
+                  label={{ value: 'Date', position: 'insideBottomRight', offset: -5 }}
+                />
+                <YAxis yAxisId="left" label={{ value: 'Lít', angle: -90, position: 'insideLeft' }} />
+                <Tooltip
+                  formatter={(value, name, props) => [`${value} Lít`, new Date(props.payload.x).toLocaleString()]}
+                />
+                <Legend />
+                <Line
+                  yAxisId="left"
+                  type="monotone"
+                  dataKey="so_lit_da_bom_rt"
+                  name="Số lít đã bơm"
+                  stroke="rgba(240, 192, 64, 1)"
+                  fill="rgba(240, 192, 64, 0.2)"
+                  dot={{ fill: 'rgba(240, 192, 64, 1)' }}
+                  activeDot={{ r: 8 }}
+                />
+              </LineChart>
+            </ResponsiveContainer>
 
-      <div className='charts'>
-        <ResponsiveContainer width="100%" height={400}>
-          <LineChart data={sensorData[config?.path[0]] || []}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="x"
-              tickFormatter={(tick) => new Date(tick).toLocaleDateString()}
-              label={{ value: 'Date', position: 'insideBottomRight', offset: -5 }}
-            />
-            <YAxis yAxisId="left" label={{ value: 'Lít', angle: -90, position: 'insideLeft' }} />
-            <Tooltip
-              formatter={(value, name, props) => [`${value} Lít`, new Date(props.payload.x).toLocaleString()]} // Displaying the value and date
-            />
-            <Legend />
-            <Line
-              yAxisId="left"
-              type="monotone"
-              dataKey="so_lit_da_bom_rt"
-              name="Số lít đã bơm"
-              stroke="rgba(240, 192, 64, 1)"
-              fill="rgba(240, 192, 64, 0.2)"
-              dot={{ fill: 'rgba(240, 192, 64, 1)' }}
-              activeDot={{ r: 8 }}
-            />
-          </LineChart>
-        </ResponsiveContainer>
-
-        <ResponsiveContainer width="100%" height={400}>
-          <LineChart data={sensorData[config?.path[0]] || []}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="x"
-              tickFormatter={(tick) => new Date(tick).toLocaleDateString()}
-              label={{ value: 'Date', position: 'insideBottomRight', offset: -5 }}
-            />
-            <YAxis yAxisId="left" label={{ value: 'Đồng', angle: -90, position: 'insideLeft' }} />
-            <Tooltip
-              formatter={(value, name, props) => [`${value} Đồng`, new Date(props.payload.x).toLocaleString()]} // Displaying the value and date
-            />
-            <Legend />
-            <Line
-              yAxisId="left"
-              type="monotone"
-              dataKey="tien_dang_ban_rt"
-              name="Tiền Đang Bán"
-              stroke="rgba(240, 192, 64, 1)"
-              fill="rgba(240, 192, 64, 0.2)"
-              dot={{ fill: 'rgba(240, 192, 64, 1)' }}
-              activeDot={{ r: 8 }}
-            />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
+            <ResponsiveContainer width="100%" height={400}>
+              <LineChart data={sensorData[path] || []}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis
+                  dataKey="x"
+                  tickFormatter={(tick) => new Date(tick).toLocaleDateString()}
+                  label={{ value: 'Date', position: 'insideBottomRight', offset: -5 }}
+                />
+                <YAxis yAxisId="left" label={{ value: 'Đồng', angle: -90, position: 'insideLeft' }} />
+                <Tooltip
+                  formatter={(value, name, props) => [`${value} Đồng`, new Date(props.payload.x).toLocaleString()]}
+                />
+                <Legend />
+                <Line
+                  yAxisId="left"
+                  type="monotone"
+                  dataKey="tien_dang_ban_rt"
+                  name="Tiền Đang Bán"
+                  stroke="rgba(240, 64, 64, 1)"
+                  fill="rgba(240, 64, 64, 0.2)"
+                  dot={{ fill: 'rgba(240, 64, 64, 1)' }}
+                  activeDot={{ r: 8 }}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+      ))}
     </main>
   );
 }
